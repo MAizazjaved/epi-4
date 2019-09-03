12dedc84c4eded2492449f7d22127b7e1d09925c
MAizazJaved <maizazjaved@hotmail.com>
2019-09-03 19:41:46 +0500

    payment unsuucessfull and welcome page created

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'EPIC 4' });
 });
+
+router.get('/payment_unsuccessful', function (req, res, next) {
+  res.render('payment_unsuccessful',{title: 'epic 4'});
+});
+
+router.get('/welcome', function (req, res, next) {
+  res.render('welcome',{title: 'epic 4'});
+});
+
+
 
 module.exports = router;
