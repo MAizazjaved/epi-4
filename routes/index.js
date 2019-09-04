36f4fddf677433800df84ead287c95e5085b8c62
MAizazJaved <maizazjaved@hotmail.com>
2019-09-04 16:05:40 +0500

    project completed

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,20 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/payment_unsuccessful', function (req, res, next) {
-  res.render('payment_unsuccessful',{title: 'epic 4'});
+  res.render('payment_unsuccessful',{title: 'Payment Unsuccessful'});
 });
 
 router.get('/welcome', function (req, res, next) {
-  res.render('welcome',{title: 'epic 4'});
+  res.render('welcome',{title: 'Welcome'});
 });
+
+router.get('/Thankyou', function (req, res, next) {
+  res.render('Thankyou',{title: 'Thankyou'});
+});
+router.get('/Action', function (req, res, next) {
+  res.render('Action',{title: 'Call to Action'});
+});
+
 
 
 
